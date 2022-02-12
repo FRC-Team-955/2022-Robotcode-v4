ff441f3927382de4312582480b30658afa9e17a6
Afernrf <91693585+Afernrf@users.noreply.github.com>
2022-02-12 14:26:56 -0800

    changes

---
+++ src/main/cpp/hopper.cpp
@@ -0,0 +1,14 @@
+#include "hopper.h"
+
+void Hopper::RunHopperMotor(float hopper_motor_percent_top,
+                            float hopper_motor_percent_bottom) {
+std::cout<<"function run" << std::endl;
+  talon_hopper_top->Set(ControlMode::PercentOutput,
+                       std::min(hopper_motor_percent_top, max));
+  talon_hopper_bottom->Set(ControlMode::PercentOutput,
+                          std::min(hopper_motor_percent_bottom, max));
+}
+void Hopper::DiplayHopperInfo(){
+  frc::Shuffleboard::GetTab("Telop").Add("Hopper Bottom Amp",talon_hopper_bottom->GetOutputCurrent());
+  frc::Shuffleboard::GetTab("Telop").Add("Hopper Top Amp",talon_hopper_top->GetOutputCurrent());
+}
---
+++ src/main/include/hopper.h
@@ -0,0 +1,24 @@
+#ifndef HOPPER
+#define HOPPER
+#include "settings.h"
+#include <algorithm>
+#include <iostream>
+#include <ctre/Phoenix.h>
+
+class Hopper {
+public:
+  Hopper(TalonSRX *talon_hopper_top, TalonSRX *talon_hopper_bottom):talon_hopper_top(talon_hopper_top), talon_hopper_bottom(talon_hopper_bottom) {
+    talon_hopper_top->TalonSRX::ConfigPeakCurrentLimit(40);
+    talon_hopper_bottom->TalonSRX::ConfigPeakCurrentLimit(40);
+    talon_hopper_top->TalonSRX::EnableCurrentLimit(true);
+    talon_hopper_bottom->TalonSRX::EnableCurrentLimit(true);
+  }
+  float max = 1;
+  void RunHopperMotor(float hopper_motor_percent_top,
+                      float hopper_motor_percent_bottom);
+  void DiplayHopperInfo();
+private:
+  TalonSRX *talon_hopper_top;
+  TalonSRX *talon_hopper_bottom;
+};
+#endif
