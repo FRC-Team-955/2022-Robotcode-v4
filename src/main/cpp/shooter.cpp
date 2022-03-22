825244f664e02e4d88d7f66f47bd113ee4fe0115
owen-mcmanus <owen.mcmanus727@gmail.com>
2022-03-22 10:10:13 -0700

    add arb feed-forward to shooter

--- src/main/cpp/shooter.cpp
+++ src/main/cpp/shooter.cpp
@@ -5,11 +5,14 @@ void Shooter::ShootPercentOutput(double top_percent, double bottom_percent) {
   shooterneo_bottom->Set(-bottom_percent);
 }
 void Shooter::VelocityControl(double top_velocity, double bottom_velocity) {
-  m_pidController_top->SetReference(top_velocity, rev::ControlType::kVelocity);
-  m_pidController_bottom->SetReference(-bottom_velocity, rev::ControlType::kVelocity);
+  // m_pidController_top->SetReference(top_velocity, rev::ControlType::kVelocity);
+  // m_pidController_bottom->SetReference(-bottom_velocity, rev::ControlType::kVelocity);
   
-  // std::cout<<double(bottom_ff->Calculate(units::revolutions_per_minute_t(shooterneo_bottom_encoder->GetVelocity()),units::revolutions_per_minute_t(-bottom_velocity))/(frc::DriverStation::GetBatteryVoltage()*5700))<<std::endl;
-  // m_pidController_bottom->SetReference(-bottom_velocity, rev::ControlType::kVelocity,0, double(bottom_ff->Calculate(units::revolutions_per_minute_t(shooterneo_bottom_encoder->GetVelocity()),units::revolutions_per_minute_t(-bottom_velocity))/(frc::DriverStation::GetBatteryVoltage()*5700)));
+  double top_arbFF = double(top_ff->Calculate(units::radians_per_second_t(top_velocity/60)))/frc::DriverStation::GetBatteryVoltage();
+  double bottom_arbFF = double(bottom_ff->Calculate(units::radians_per_second_t(-bottom_velocity/60)))/frc::DriverStation::GetBatteryVoltage();
+  // double bottom_arbFF = double(bottom_ff->Calculate(units::radians_per_second_t(shooterneo_bottom_encoder->GetVelocity()/60),units::radians_per_second_t(bottom_velocity/60),timer_shooter->Get()))/frc::DriverStation::GetBatteryVoltage();
+  m_pidController_top->SetReference(top_velocity, rev::ControlType::kVelocity,0,top_arbFF);
+  m_pidController_bottom->SetReference(-bottom_velocity, rev::ControlType::kVelocity,0,bottom_arbFF);
 }
 float Shooter::VelocityOutput(std::string shooter_motor){
   if (shooter_motor == "Top"){
